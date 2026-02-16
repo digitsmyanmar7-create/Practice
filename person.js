2133be0c1bf70059325a6856b5c8fdc4137ed2a5
digitsmyanmar7-create <digitsmyanmar7@gmail.com>
2026-02-16 19:15:36 +0630

    2

--- person.js
+++ person.js
@@ -3,3 +3,18 @@ const age = 40;
 
 // export name and age
 export { name, age };
+
+export const PI = 3.14159;
+
+export function add(a, b) {
+  return a + b;
+}
+export function subtract(a, b) {
+  return a - b;
+}
+export function multiply(a, b) {
+  return a * b;
+}
+export function divide(a, b) {
+  return a / b;
+} ;
--- script.js
+++
@@ -1,9 +0,0 @@
-import { name, age } from "./person.js";
-
-let c =  name + age ;
-
-
-
-
-
-console.log ( c )
