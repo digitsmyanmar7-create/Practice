7814785fc52fc61388753b9730f8d53e65e9b894
digitsmyanmar7-create <digitsmyanmar7@gmail.com>
2026-02-17 21:56:42 +0630

    2

--- practice.js
+++ practice.js
@@ -1,3 +1,9 @@
-/* Asynchronous Programming */
+/* Async /The setTimeout() Method  */
 
 
+
+setTimeout(myFunction, 3000);
+
+function myFunction() {
+ console.log ( "https://www.w3schools.com/js/js_async_timeouts.asp" );
+};
