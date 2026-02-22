b5c13abbc35f3f61b982b643fa943128b6e037e2
digitsmyanmar7-create <digitsmyanmar7@gmail.com>
2026-02-22 20:06:38 +0630

    one

--- practice.js
+++ practice.js
@@ -1,9 +1,20 @@
-/* Async /The setTimeout() Method  */
+// Mom making a promise to give cookies
+const cookiePromise = new Promise((resolve, reject) => {
+  // Baking takes time
+  setTimeout(() => {
+    let cookiesReady = true;
+    
+    if (cookiesReady) {
+      resolve("🍪 Here are your cookies!");
+    } else {
+      reject("😢 Sorry, no cookies today");
+    }
+  }, 5000); // Takes 5 seconds to bake
+});
 
-
-
-setTimeout(myFunction, 3000);
-
-function myFunction() {
- console.log ( "https://www.w3schools.com/js/js_async_timeouts.asp" );
-};
+// You can go play while waiting
+console.log("Going to play!");
+cookiePromise
+  .then((result) => console.log(result))  // Yay! Cookies!
+  .catch((error) => console.log(error)); // Aw, no cookies
+console.log("Playing with toys...");
